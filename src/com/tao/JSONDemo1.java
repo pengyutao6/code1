99874fab223104c23ec19f9471d43bf92703a32e
tao <fengtao9474@126.com>
2023-05-25 09:29:23 +0800

    change

--- src/com/tao/JSONDemo1.java
+++ src/com/tao/JSONDemo1.java
@@ -9,6 +9,7 @@ public class JSONDemo1 {
     public static void main(String[] args) {
         JSONDemo1 jsonDemo1 = new JSONDemo1();
         System.out.println(jsonDemo1.name.getClass().getFields()[0]);
+        System.out.println("TestPush,Gitee");
     }
 
     public static String getSerializedKey(Field field){
