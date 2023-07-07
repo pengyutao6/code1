101fcf4fb991b330a2d79af44ebc6ed11d695314
tao <fengtao9474@126.com>
2023-07-07 16:38:53 +0800

    change

--- src/com/tao/JSONDemo1.java
+++ src/com/tao/JSONDemo1.java
@@ -4,10 +4,11 @@ import java.lang.reflect.Field;
 
 public class JSONDemo1 {
     @Demo1.JsonField("tao")
-    private String name;
+    private String name = "tao";
 
     public static void main(String[] args) {
         JSONDemo1 jsonDemo1 = new JSONDemo1();
+
         System.out.println(jsonDemo1.name.getClass().getFields()[0]);
         System.out.println("TestPush,Gitee");
     }
