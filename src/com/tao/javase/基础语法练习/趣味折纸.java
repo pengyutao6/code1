ee2b0e9869897ddeede288222c18a2b9a2258dc2
linfengtao <66399287+linfengtao@users.noreply.github.com>
2023-07-09 14:11:47 +0800

    demo

---
+++ src/com/tao/javase/基础语法练习/趣味折纸.java
@@ -0,0 +1,16 @@
+package com.tao.javase.基础语法练习;
+
+public class 趣味折纸 {
+    public static void main(String[] args) {
+        //世界最高山峰是珠穆朗玛峰，它的高度是8848.86米，假如我有一张足够大的纸，它的厚度是0.1毫米。请问，我折叠多少次，可以折成珠穆朗玛峰的高度?
+        int count = 0;
+        int height = (int)8848.86*1000;
+
+        for (double i = 0.1; i < height; i*=2) {
+            count +=1;
+        }
+        //27次
+        System.out.println("折叠次数："+count);
+    }
+
+}
