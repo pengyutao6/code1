c137a88d8a7eccb0b425ef8421d9dea37efe77a8
pengyutao6 <fengtao9474@126.com>
2023-07-11 10:21:00 +0800

    update

--- src/com/tao/javase/基础语法练习/二分查找法.java
+++ src/com/tao/javase/基础语法练习/二分查找法.java
@@ -7,18 +7,24 @@ public class 二分查找法 {
      * @param target 目标值
      * @return -1表示未找到
      */
-    public int binarySearch(int[] nums,int target){
+    public int binarySearch2(int[] nums,int target){
+        //左指针
         int left = 0;
-        int right = nums.length - 1;
+        //右指针
+        int right = nums.length-1;
         while (left<=right){
-            //中间值
             int mid = left + (left+right)/2;
-            if (nums[mid] == target){
+            //找到目标值，返回下标值
+            if(nums[mid]==target){
                 return mid;
-            }else if(nums[mid] < target){
+                //如果目标值大于中间值，那么说明目标值可能在右侧区域
+                //所以让左指针往右折
+            }else if(nums[mid]<target){
                 left = mid + 1;
-            }else {
-                right = mid - 1;
+            }else{
+                //如果目标值小于中间值，那么说明目标值可能在左侧区域
+                //所以让右指标往左折
+                right = mid -1;
             }
         }
         return -1;
--- src/com/tao/javase/基础语法练习/冒泡排序.java
+++ src/com/tao/javase/基础语法练习/冒泡排序.java
@@ -1,4 +1,4 @@
-package com.tao;
+package com.tao.javase.基础语法练习;
 
 public class 冒泡排序 {
 
@@ -11,6 +11,24 @@ public class 冒泡排序 {
     }
 
     public static int[] bubbleSort(int[] arr){
+        int length = arr.length;
+        for (int i = 0; i < length-1; i++) {
+            for (int j = 0; j < length-i-1; j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+    /*
+        两个for循环，一个是每一个元素循环从左右到右遍历比较每个元素
+                一个是遍历每个元素进行上面的比较
+            然后在最里面的循环，进行元素比较，如果左边大于右边，则进行位置替换
+     */
+    public static int[] bubbleSort2(int[] arr){
         int length = arr.length;
         for (int i = 0; i < length-1; i++) {
             for (int j = 0; j < length-i-1; j++) {
